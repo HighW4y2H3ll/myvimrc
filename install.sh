541b4daecfbdd962335e441b8c27e8804f3948da
hzh <huzh@nyu.edu>
2018-10-05 16:37:40 +0000

    install ruby version > 2.0 by rbenv

--- install.sh
+++ install.sh
@@ -63,6 +63,20 @@ rm -rf tmp_codequery
 
 # Other ctags/cscope tools
 sudo pip install pycscope   # Python
-sudo apt-get install ruby-full
+
+# Install rbenv for ruby > 2.0
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev \
+    libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+pushd $HOME/
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+rbenv global 2.2.3
 sudo gem install starscope  # Javascript, Ruby, Go
+
+popd
 
